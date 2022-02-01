b1dde8171b7caa035af1bd4da6995ebea5b92cc4
Bozhidar Savov <scrollsoflankhmar@gmail.com>
2022-02-01 16:45:25 +0200

    add javascript, script files and first event listeners

---
+++ scripts/app.js
@@ -0,0 +1,5 @@
+const editPlayer1BtnEl = document.getElementById("edit-player-1-btn");
+const editPlayer2BtnEl = document.getElementById("edit-player-2-btn");
+
+editPlayer1BtnEl.addEventListener("click", openPlayerConfig);
+editPlayer2BtnEl.addEventListener("click", openPlayerConfig);
