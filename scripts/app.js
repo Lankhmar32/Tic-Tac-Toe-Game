7795a5ac7ca3b1de8663757049a5947bc342d8ab
Bozhidar Savov <scrollsoflankhmar@gmail.com>
2022-02-01 16:56:58 +0200

    showing and hiding the config modal overlay

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,13 @@
+const playerConfigOverlayEl = document.getElementById("config-overlay");
+const backdropEl = document.getElementById("backdrop");
+
 const editPlayer1BtnEl = document.getElementById("edit-player-1-btn");
 const editPlayer2BtnEl = document.getElementById("edit-player-2-btn");
 
+const cancelConfigbtnEl = document.getElementById("cancel-config-btn");
+
 editPlayer1BtnEl.addEventListener("click", openPlayerConfig);
 editPlayer2BtnEl.addEventListener("click", openPlayerConfig);
+
+cancelConfigbtnEl.addEventListener("click", closePlayerConfig);
+backdropEl.addEventListener("click", closePlayerConfig);
--- scripts/config.js
+++ scripts/config.js
@@ -1,1 +1,11 @@
-function openPlayerConfig() {}
+"use strict";
+
+function openPlayerConfig() {
+  playerConfigOverlayEl.style.display = "block";
+  backdropEl.style.display = "block";
+}
+
+function closePlayerConfig() {
+  playerConfigOverlayEl.style.display = "none";
+  backdropEl.style.display = "none";
+}
