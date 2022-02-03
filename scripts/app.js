5c8c49badc7bb5e34177ed64744a78e4a6d499a4
Bozhidar Savov <scrollsoflankhmar@gmail.com>
2022-02-03 15:47:10 +0200

    manage field selection and game rounds

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,7 @@
 "use strict";
 
 let editedPlayer = 0;
+let activePlayer = 0;
 
 const players = [
   { name: "", symbol: "X" },
@@ -12,11 +13,15 @@ const backdropEl = document.getElementById("backdrop");
 const fromEl = document.querySelector("form");
 const errorsOutputEl = document.getElementById("config-error");
 const gameAreaEl = document.getElementById("active-game");
+const activePlayerName = document.getElementById("active-player-name");
 
 const editPlayer1BtnEl = document.getElementById("edit-player-1-btn");
 const editPlayer2BtnEl = document.getElementById("edit-player-2-btn");
 const cancelConfigbtnEl = document.getElementById("cancel-config-btn");
 const startNewGameBtn = document.getElementById("start-game-btn");
+
+// const gameFieldElements = document.querySelectorAll("#game-board li");
+const gameBoardEl = document.getElementById("game-board");
 
 editPlayer1BtnEl.addEventListener("click", openPlayerConfig);
 editPlayer2BtnEl.addEventListener("click", openPlayerConfig);
@@ -27,3 +32,9 @@ backdropEl.addEventListener("click", closePlayerConfig);
 fromEl.addEventListener("submit", savePlayerConfig);
 
 startNewGameBtn.addEventListener("click", startNewGame);
+
+// for (const gameFieldEl of gameFieldElements) {
+//   gameFieldEl.addEventListener("click", selectGameField);
+// }
+
+gameBoardEl.addEventListener("click", selectGameField);
--- scripts/game.js
+++ scripts/game.js
@@ -6,5 +6,26 @@ function startNewGame() {
     return;
   }
 
+  activePlayerName.textContent = players[activePlayer].name;
   gameAreaEl.style.display = "block";
+}
+
+function switchPlayer() {
+  if (activePlayer === 0) {
+    activePlayer = 1;
+  } else {
+    activePlayer = 0;
+  }
+  activePlayerName.textContent = players[activePlayer].name;
+}
+
+function selectGameField(event) {
+  // console.log(event.target.tagName);
+  if (event.target.tagName !== "LI") {
+    return;
+  }
+
+  event.target.textContent = players[activePlayer].symbol;
+  event.target.classList.add("disabled");
+  switchPlayer();
 }
